3d2ca2de07a9df0763ab234deb37f329de1f8ec2
Alexandre Gueths <agueths@gmail.com>
2025-03-28 09:58:28 -0300

    Update update_projects.sh

--- update_projects.sh
+++ update_projects.sh
@@ -15,7 +15,7 @@ do
     [[ -z "$name" || "$name" == "#"* ]] && continue
 
     echo "=============================="
-    echo "Processing: $name"
+    echo "Checking: $name"
     echo "=============================="
 
     PROJECT_DIR="$BASE_DIR/$folder"
@@ -23,14 +23,10 @@ do
     if [ ! -d "$PROJECT_DIR" ]; then
         echo "Cloning repository from $project_git para $PROJECT_DIR..."
         git clone "$project_git" "$PROJECT_DIR"
-    else
-        echo "Project $name already exists. Updating..."
-        cd "$PROJECT_DIR" || exit
-        git pull origin development
     fi
 
     # Verificar se o script update_container.sh existe
-    UPDATE_SCRIPT="#PROJECT_DIR/script/update_container.sh"
+    UPDATE_SCRIPT="$PROJECT_DIR/script/update_container.sh"
 
     if [ -f "$UPDATE_SCRIPT" ]; then
         echo "Running update script for $name..."
